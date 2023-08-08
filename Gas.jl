1714f439481fc45d0b3747f4a6f921ef80c737a3
Prashanth Prakash <prash@mit.edu>
2023-08-08 16:29:25 -0400

    clean up gas show

--- Gas.jl
+++ Gas.jl
@@ -76,6 +76,15 @@ function Base.show(io::IO, gas::Gas)
    @printf(io, "Ideal Gas at\n%3s = %8.3f K\n%3s = %8.3f kPa\n%3s = %8.3f J/K/mol\n%3s = %8.3f kJ/mol\n%3s = %8.3f kJ/K/mol",
      "T", gas.T, "P", gas.P/1000.0, "cp", gas.cp, "h", gas.h/1000.0, "s", gas.s/1000.0)
    println("\n\nwith composition:")
+   composition(gas,io)
+end
+
+"""
+    composition(gas::Gas, io::IO=stdout)
+
+Prints out the composition (Y_i) of the gas
+"""
+function composition(gas::Gas, io::IO=stdout)
    divider = "-"^(8*2+4+9)
    @printf(io, "%s\n", divider)
    @printf(io, "%8s  %8s  %9s\n", "Species", "Yᵢ", "MW[g/mol]")
@@ -86,7 +95,7 @@ function Base.show(io::IO, gas::Gas)
       end
    end
    @printf(io, "%s\n", divider)
-   @printf(io, "%8s  %8.3f  %9.3f\n", " ", sum(gas.Y), gas.MW)
+   @printf(io, "%8s  %8.3f  %9.3f\n", "ΣYᵢ", sum(gas.Y), gas.MW)
 end
 
 # Automatically calculates the Tarray if T is set
