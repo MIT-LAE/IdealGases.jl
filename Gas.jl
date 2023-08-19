560591d60d1dffb4b5f9fab9e8bbd4ff839e1885
Prashanth Prakash <prash@mit.edu>
2023-08-19 15:55:29 -0400

    move thermotable to utils

--- Gas.jl
+++ Gas.jl
@@ -474,45 +474,6 @@ function compress(gas::Gas, PR::Float64, ηp::Float64=1.0,)
 
 end
 
-"""
-    thermo_table(gas::Gas, 
-    Tstart::Float64=Tstd, Tend::Float64=2000.0, Tinterval::Float64=100.0,
-    print::Bool=true)
-
-Quickly generate a table of cp, h and s for a gas
-"""
-function thermo_table(gas::Gas; 
-    Tstart::Float64=Tstd, Tend::Float64=2000.0, Tinterval::Float64=100.0,
-    print::Bool=false)
-    
-   Trange = range(Tstart, Tend, step=Tinterval)
-
-   cp_array = zero(Trange)
-   h_array  = zero(Trange)
-   ϕ_array  = zero(Trange)
-   if print
-      composition(gas)
-      println(" ")
-      divider = "-"^(4+8+12*3+4)
-      @printf("%4s %8s %12s %12s %12s\n",
-            "i",  "T[K]", "cp[J/K/mol]", "h[kJ/mol]", "s[kJ/K/mol]")
-      println(divider)
-      for (i,T) in enumerate(Trange)
-         gas.T = T
-         @printf("%4d %8.2f %12.4f %12.4f %12.4f\n",
-         i,  gas.T, gas.cp, gas.h/1000.0, gas.s/1000.0)
-      end
-   else
-      for (i,T) in enumerate(Trange)
-         gas.T = T
-         cp_array[i]= gas.cp
-         h_array[i] =  gas.h
-         ϕ_array[i] =  gas.𝜙
-      end
-   return Trange, cp_array, h_array, ϕ_array
-   end
-end
-
 Y = Dict(  
 "N2"  => 0.78084,
 "Ar"  => 0.009365,
