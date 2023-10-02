13d1b5b0cc5352f07994016928260ea8ccfbbbb6
Prashanth Prakash <prash@mit.edu>
2023-10-02 12:59:38 -0400

    doc update

--- src/readThermo.jl
+++ src/readThermo.jl
@@ -1,10 +1,13 @@
 using StructArrays
 """
+    readThermo(filename)
+
 Reads a NASA 9 polynomial thermo definintion file which can be obtained from
 [NASA thermobuild](https://cearun.grc.nasa.gov/ThermoBuild/index_ds.html) 
 and returns a dictionary of species.
 
-See https://shepherd.caltech.edu/EDL/PublicResources/sdt/formats/nasa.html for typical data format
+See [here](https://shepherd.caltech.edu/EDL/PublicResources/sdt/formats/nasa.html) 
+for typical data format
 
 Usage:
 If a NASA 9 polynomial definition file `thermo.inp` exists then,
