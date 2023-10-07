d1a3eef144c75a9abaaac33af59482924ed0b1a5
Prashanth Prakash <prash@mit.edu>
2023-10-07 10:50:20 -0400

    add wider range of tests for composites

--- test/unit_test_composite.jl
+++ test/unit_test_composite.jl
@@ -8,6 +8,10 @@
     Air = species_in_spdict("Air")
     composite_air = IdealGases.generate_composite_species(gas.X, "air")
 
+    @test IdealGases.Cp(T, composite_air) ≈ gas.cp
+    @test IdealGases.h(T, composite_air) ≈ gas.h
+    @test IdealGases.s(T, P, composite_air) ≈ gas.s
+
     @test composite_air.MW ≈ Air.MW atol=1e-7 #that's the no. of decimals in thermo.inp
     @test composite_air.Hf ≈ Air.Hf atol=1e-2 #that's the no. of decimals in thermo.inp
     for i in eachindex(Air.alow)
@@ -22,4 +26,14 @@
     T *= 5
     @test IdealGases.s(T, P, Air) ≈ IdealGases.s(T, P, composite_air) rtol=1e-8
     
+    for Ti in rand(200.0:2000.0,5)
+        gas.T = Ti
+        @test gas.cp ≈ IdealGases.Cp(Ti, composite_air)
+        @test gas.h ≈ IdealGases.h(Ti, composite_air)
+        for Pi in rand(101325.0:5*101325.0,5)
+            gas.P = Pi
+            @test gas.s≈ IdealGases.s(Ti, gas.P, composite_air)
+            @test IdealGases.s(gas.TP..., Air) ≈ IdealGases.s(gas.TP..., composite_air)
+        end
+    end
 end
