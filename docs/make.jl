c0765532ee594f9cc20285c6e00982f092b7e3d4
Prashanth Prakash <prash@mit.edu>
2023-10-07 17:23:24 -0400

    update make

--- docs/make.jl
+++ docs/make.jl
@@ -7,12 +7,28 @@ push!(LOAD_PATH, "../src")
 using Documenter
 
 makedocs(
-    remotes = nothing,
-    sitename = "IdealGases.jl documentation",
-    pages = ["Home" =>"index.md",
-    "Thermodynamic Data" => "readthermo.md",
-    "Gas type" => "gas.md",
-    "Ideal gas thermo" => "idealgasthermo.md",
-    "Combustion"=>"combustion.md"]
+    remotes=nothing,
+    sitename="IdealGases.jl documentation",
+    pages=["Home" => "index.md",
+        "Thermodynamic Data" => "readthermo.md",
+        "Gas and Species" => "gas.md",
+        "Ideal gas thermo" => "idealgasthermo.md",
+        "Combustion" => "combustion.md"], 
 
+    format = Documenter.HTML(; mathengine=
+        Documenter.KaTeX(
+            Dict(:delimiters => [
+                Dict(:left => raw"$",   :right => raw"$",   display => false),
+                Dict(:left => raw"$$",  :right => raw"$$",  display => true),
+                Dict(:left => raw"\[",  :right => raw"\]",  display => true),
+                ],
+                :macros => 
+                Dict("\\RR" => "\\mathbb{R}",
+                    "\\genfuel" => 
+                    raw"{\mathrm{C}_{x_{\mathrm{C}}}\mathrm{H}_{x_{\mathrm{H}}}\mathrm{O}_{x_{\mathrm{O}}}\mathrm{N}_{x_{\mathrm{N}}}}",
+                    raw"\fst" => raw"f_{\mathrm{stoich.}}",
+                ),
+            )
+        )
+    )
 )
