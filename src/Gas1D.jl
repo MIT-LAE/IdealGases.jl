48aa64f9b351ca3bf9164dbc64e8960d97bcb709
Prashanth Prakash <prash@mit.edu>
2023-10-08 22:42:28 -0400

    define gas1D type

---
+++ src/Gas1D.jl
@@ -0,0 +1,81 @@
+
+mutable struct Gas1D <: AbstractGas
+    comp_sp::composite_species
+    P::Float64 # [Pa]
+    T::Float64 # [K]
+    Tarray::MVector{8, Float64} # Temperature array to make calcs allocation free
+ 
+    cp::Float64 #[J/kg/K]
+    cp_T::Float64 # derivative dcp/dT
+    h::Float64  #[J/kg]
+    ϕ::Float64  #[J/kg/K] Entropy complement fn ϕ(T) = ∫ cp(T)/T dT from Tref to T
+    #Intentionally not storing s since log is expensive so only calculated when requested
+
+end
+
+function Gas1D()
+    Gas1D(DryAir, Pstd, Tstd, Tarray(Tstd), 
+    Cp(Tstd, DryAir), 
+    (Cp(Tstd + 1.0, DryAir) - Cp(Tstd - 1.0, DryAir)) /2.0,
+    h(Tstd, DryAir),
+    s(Tstd, Pstd, DryAir))
+end
+
+function Base.setproperty!(gas::Gas1D, sym::Symbol, val::Float64)
+    ## Setting Temperature
+    if sym === :T
+       setfield!(gas, :T, val) # first set T
+       setfield!(gas, :Tarray, Tarray!(val, getfield(gas, :Tarray))) # update Tarray
+       TT = view(getfield(gas, :Tarray), :) # Just convinence
+       # Next set the cp, h and s of the gas
+       ## Get the right coefficients 
+       ## (assumes Tmid is always 1000.0. Check performed in readThermo.jl.):
+       sp = getfield(gas, :comp_sp)
+       if val<1000.0 #Value given is the desired temperature
+          A = view(sp.alow, :)
+       else
+          A = view(sp.ahigh, :)
+       end   
+ 
+       P = getfield(gas, :P)
+       MW = sp.MW/1000.0 # kg/mol
+
+       setfield!(gas, :cp, Cp(TT,A)/MW)
+       setfield!(gas, :h, h(TT, A)/MW)
+       setfield!(gas, :ϕ, 𝜙(TT, A)/MW)
+       setfield!(gas, :cp_T, dCpdT(TT, A)/MW)
+ 
+    ## Setting Pressure
+    elseif sym === :P
+       setfield!(gas, :P, val)
+    elseif sym ===:h
+       set_h!(gas, val)
+    elseif sym === :TP
+       set_TP!(gas, val[1], val[2])
+    else
+       error("You tried setting gas.", sym, " to a ", typeof(val))
+    end
+    # Note: intentionally not including other variables to prevent 
+    # users from trying to directly set h, s, cp, MW etc.
+    nothing
+ end
+
+ # Overload Base.getproperty for convinence
+function Base.getproperty(gas::Gas1D, sym::Symbol)
+    if sym === :h_T # dh/dT
+       return getfield(gas, :cp)
+    elseif sym === :ϕ_T # dϕ/dT
+       return getfield(gas, :cp)/getfield(gas, :T)
+    elseif sym === :s_T # ∂s/∂T = dϕ/dT
+       return getfield(gas, :cp)/getfield(gas, :T)
+    elseif sym === :hs
+       return [getfield(gas, :h), getfield(gas, :s)]
+    elseif sym === :TP
+       return [getfield(gas, :T), getfield(gas, :P)]
+    elseif sym === :s
+       Rgas = Runiv/getfield(gas, :comp_sp).MW*1000.0
+       return getfield(gas, :ϕ) - Rgas*log(getfield(gas,:P)/Pstd)
+    else
+       return getfield(gas, sym)
+    end
+ end
